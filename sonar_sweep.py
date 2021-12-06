55fb918794e9c471b70554fda498d8fc57dd8f30
samirm <samir.m8@gmail.com>
2021-12-05 23:24:54 -0500

    day 1, refactor

--- sonar_sweep.py
+++ sonar_sweep.py
@@ -2,13 +2,13 @@
 with open('data/depths.txt') as file:
     lines = file.readlines()
 
-    firstValue = lines[0]
-    lastDepth = int(lines[0]) + int(lines[1]) + int(lines[2])
+    first_value = lines[0]
+    last_depth = int(lines[0]) + int(lines[1]) + int(lines[2])
     increase = 0
 
     for index, item in enumerate(lines[1:-2]):
-        slidingWindow = int(lines[index + 1]) + int(lines[index + 2]) + int(lines[index + 3])
-        if slidingWindow > lastDepth: increase += 1
-        lastDepth = slidingWindow
+        sliding_window = int(lines[index + 1]) + int(lines[index + 2]) + int(lines[index + 3])
+        if sliding_window > last_depth: increase += 1
+        last_depth = sliding_window
 
     print(increase)
